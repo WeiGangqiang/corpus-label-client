c2089dfb58d44d37458bd8ab0256f554c564333a
杨国伟 <951014483@qq.com>
2018-08-11 21:29:15 +0800

    删除没用代码

--- app/pages/intendList.js
+++ app/pages/intendList.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux'
 import { Link } from 'react-router'
 import { Spin, Icon, Form, Row, Col} from 'antd'
 import { isArrayDomain } from 'utils/util'
-import { fetchIntend, fetchEntity,postPattern, postCorpus, simplifier, predict, getPhrase, putPhrase, deletePhrase, postPhrase } from 'actions/intend'
+import { fetchIntend, fetchEntity,postPattern, postCorpus, predict, getPhrase, putPhrase, deletePhrase, postPhrase } from 'actions/intend'
 
 import { PatternList, PhraseList, EntityParameters, IntentList, CorpusSimplifier } from "components/index";
 
@@ -25,25 +25,13 @@ export default class intendList extends Component {
     this.state = {
       loading: false,
       originEntity: [],
-      contents:[],
-      content: '',
       value:'',
-      intentId: '1',
-      signWord: '',
-      signWords: [],
-      replaceWords: [],
+      intentId: '',
       name: '',
       zhName: '',
       modelPath: '',
-      newCorpus: '',
-      simCorpus: '',
       entityParam: [],
       phraseArray: [],
-      showModalFlag: false,
-      showModalPatternFlag: false,
-      phraseText: '',
-      phraseIndex:'',
-      contentIndex: '',
       type: 'positive'
     };
     this.initData = this.initData.bind(this);
@@ -56,7 +44,6 @@ export default class intendList extends Component {
 
   }
   componentWillMount() {
-    // 请求相应的预料，对signWord进行赋值等等
     const agentName = sessionStorage.getItem('agentName');
     this.props.dispatch(fetchIntend('?agent=' + agentName, data =>{
       if(data.length){
@@ -72,11 +59,6 @@ export default class intendList extends Component {
   componentDidMount() {
   }
   getIntent(item,index) {
-    this.setState({
-      contents:[],
-      signWords: [],
-
-    })
     this.initData(item)
   }
   initData(obj) {
@@ -125,32 +107,6 @@ export default class intendList extends Component {
           console.log(error)
         }))
   }
-  showSignWord(arr,word) {
-    let showSign='';
-    if(arr){
-      for(let i=0;i<arr.length;i++){
-        showSign = showSign + word.substring(0,arr[i].signWordStart) + '<span class="corpusSpan" style="background: '+ arr[i].color+'">' + word.substring(arr[i].signWordStart,arr[i].signWordEnd) + '</span>' + word.substring(arr[i].signWordEnd)
-        word = showSign;
-        showSign = ''
-    }
-    }
-    return word
-  }
-
-  simplifier() {
-    this.props.form.validateFields((err, values) => {
-      if (!err) {
-        this.props.dispatch(simplifier({x:values.newCorpus}, data => {
-          console.log(data)
-          this.setState({
-            simCorpus:data.y
-          })
-        }, err => {
-          console.log(err)
-        }))
-      }
-    })
-  }
 
   useSimCorpus(simCorpus) {
     this.props.dispatch(postPattern({
@@ -164,28 +120,10 @@ export default class intendList extends Component {
       agent:agentName
   }))
     this.props.dispatch(predict({
-      "sentence" : this.state.simCorpus,
+      "sentence" : simCorpus,
       "intentId" : this.state.intentId,
       "agent"    : agentName
     },data => {
-    //   const content = {
-    //     pattern: {
-    //       sentence: this.state.simCorpus,
-    //       labels: data
-    //     },
-    //     type: "positive",
-    //     intentId: this.state.intentId,
-    //     intent: this.state.name,
-    //     agent: agentName
-    // }
-    //   this.setState({
-    //     contents:[...this.state.contents, content],
-    //     newCorpus: '',
-    //     simCorpus: ''
-    //   });
-    //   this.props.form.setFieldsValue({
-    //     newCorpus: ''
-    //   })
     }, error => {
       console.log(error)
     }))
@@ -203,28 +141,10 @@ export default class intendList extends Component {
       agent:agentName
     }))
     this.props.dispatch(predict({
-      "sentence" : this.state.newCorpus,
+      "sentence" : newCorpus,
       "intentId" : this.state.intentId,
       "agent"    : agentName
     },data => {
-      // const content = {
-      //   pattern: {
-      //     sentence: this.state.newCorpus,
-      //     labels: data
-      //   },
-      //   type: "positive",
-      //   intentId: this.state.intentId,
-      //   intent: this.state.name,
-      //   agent: agentName
-      // }
-      // this.setState({
-      //   contents:[...this.state.contents, content],
-      //   newCorpus: '',
-      //   simCorpus: ''
-      // });
-      // this.props.form.setFieldsValue({
-      //   newCorpus: ''
-      // })
     }, error => {
       console.log(error)
     }))
@@ -248,70 +168,12 @@ export default class intendList extends Component {
     const { intendResult } = this.props;
 
     const style = {
-      container: {
-        background: '#fff',
-      },
-      headerTitle:{
-        lineHeight: '40px',
-        fontSize: '16px',
-        float: 'left',
-        marginTop: '-40px',
-
-      },
-      flexBox: {
-        display: 'flex',
-        flexWrap: 'wrap',
-        justifyContent: 'space-start',
-        flexDirection: 'column'
-      },
-      serveLi: {
-        padding: '5px 0px',
-        fontSize: '14px',
-      },
-      serveLiSpan: {
-        marginRight: '15px',
-        padding: '5px 10px',
-        borderRadius: '3px',
-        cursor: 'pointer',
-        display: 'inline-block',
-        marginBottom: '15px'
-      },
-      pBox:{
-        position: 'relative',
-        border: '1px solid #dadada',
-      },
-      p: {
-        background: '#fcfcfc',
-        color: '#333',
-        fontSize: '16px',
-        textAlign: 'center',
-        lineHeight: '100px',
-      },
-      button:{
-        fontSize: '16px',
-        padding: '8px 12px',
-        lineHeight: '16px',
-        borderRadius: '3px',
-        border: '1px solid #dadada',
-        display: 'inline-block',
-        marginLeft: '20px',
-        color: '#fff',
-        cursor: 'pointer'
-      },
-      funBox:{
-        textAlign: 'right',
-        marginTop: '20px',
-      },
       corpusBox:{
         background: '#fbfbfb',
         padding: '15px',
         width: '100%',
         height: '100%',
         borderRadius: '15px'
-      },
-      intendBox:{
-        height: '100%',
-        float: 'left',
       },
       innerContainer:{
         width: '100%',
@@ -321,51 +183,9 @@ export default class intendList extends Component {
       innerBox:{
         height: '100%'
       },
-      newCorpusBox:{
-        width: '300px',
-        lineHeight: '32px',
-        display: 'inline-block',
-        border: '1px solid #dadada',
-        borderRadius: '4px',
-        paddingLeft: '15px',
-        fontSize:'14px'
-      },
       col:{
         lineHeight: '40px'
       },
-      phraseBox:{
-        marginTop: '15px'
-      },
-      phraseItem:{
-        background: '#fbfbfb',
-        paddingLeft: '10px'
-      },
-      phraseText:{
-        display: 'inline-block',
-        padding: '5px 10px',
-        margin: '7px',
-        marginRight: '15px',
-        background: '#ccc',
-        color: '#fff',
-        fontSize: '14px',
-        borderRadius: '3px'
-      },
-      corpusSpan:{
-        width: '20px',
-        textAlign: 'center',
-        marginLeft: '-20px',
-        float: 'left',
-      },
-      corpusCol:{
-        paddingLeft: '20px',
-        paddingRight: '20px'
-      },
-      phraseDiv:{
-        width: '20px',
-        textAlign: 'center',
-        marginRight: '-20px',
-        float: 'right',
-      }
     };
     console.log('this.state.intentId', this.state.intentId)
 
