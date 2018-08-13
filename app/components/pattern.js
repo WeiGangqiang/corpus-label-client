e6f267921cce745c9898d987127ef8566c7aaff3
杨国伟 <951014483@qq.com>
2018-08-13 18:03:35 +0800

    fix bug

--- app/components/pattern.js
+++ app/components/pattern.js
@@ -3,132 +3,133 @@ import {Button, Row, Col} from 'antd'
 import { ColorDownList } from "./colorDownList";
 
 export class PatternLine extends Component {
-    constructor(props) {
-        super(props)
-      this.state={
-        showDownlist: false,
-        top: 0,
-        left: 0
+  constructor(props) {
+    super(props)
+    this.state={
+      showDownlist: false,
+      top: 0,
+      left: 0
+    }
+  }
+
+  getSpanStyleBy = (type) => {
+    const style = {
+      entity: {
+        background: 'blue',
+        color: '#fff',
+        padding: '5px 10px',
+        borderRadius: '3px'
+      },
+      similar: {
+        background: 'green',
+        color: '#fff',
+        padding: '5px 10px',
+        borderRadius: '3px'
       }
     }
-     
-    getSpanStyleBy = (type) => {
-        const style = {
-            entity: {
-                background: 'blue',
-                color: '#fff',
-                padding: '5px 10px',
-              borderRadius: '3px'
-            },
-            similar: {
-                background: 'green',
-              color: '#fff',
-              padding: '5px 10px',
-              borderRadius: '3px'
-            }
-        }
-        return  (type == 'entity') ? style.entity : style.similar
-    }
+    return  (type == 'entity') ? style.entity : style.similar
+  }
 
-    getSpans = () => {
-        let sentence = this.props.pattern.sentence
-        let labels = this.props.pattern.labels
-        let startPos = 0
-        let spans = []
-        labels.forEach(label => {
-            if(startPos < label.startPos){
-                spans.push(<span key={spans.length} id={spans.length}>{sentence.slice(startPos, label.startPos)}</span>) 
-            }
-            spans.push(<span key={spans.length} id={spans.length} style ={this.getSpanStyleBy(label.type)}>{sentence.slice(label.startPos, label.startPos + label.length)}</span>)
-            startPos = label.startPos + label.length
-        })
-        if (startPos < sentence.length){
-            spans.push(<span key={spans.length} id={spans.length}>{sentence.slice(startPos)}</span>)
-        }
-        return spans
+  getSpans = () => {
+    let sentence = this.props.pattern.sentence
+    let labels = this.props.pattern.labels
+    let startPos = 0
+    let spans = []
+    labels.forEach(label => {
+      if(startPos < label.startPos){
+        spans.push(<span key={spans.length} id={spans.length}>{sentence.slice(startPos, label.startPos)}</span>)
+      }
+      spans.push(<span key={spans.length} id={spans.length} style ={this.getSpanStyleBy(label.type)}>{sentence.slice(label.startPos, label.startPos + label.length)}</span>)
+      startPos = label.startPos + label.length
+    })
+    if (startPos < sentence.length){
+      spans.push(<span key={spans.length} id={spans.length}>{sentence.slice(startPos)}</span>)
     }
+    return spans
+  }
 
-    getSpanStartPos = () => {
-        let sentence = this.props.pattern.sentence
-        let labels = this.props.pattern.labels
-        let startPos = 0
-        let spanStarts = []
-        labels.forEach(label => {
-            if(startPos < label.startPos){
-                spanStarts.push(startPos) 
-            }
-            spanStarts.push(label.startPos)
-            startPos = label.startPos + label.length
-        })
-        if (startPos < sentence.length){
-            spanStarts.push(startPos)
-        }
-        return spanStarts
+  getSpanStartPos = () => {
+    let sentence = this.props.pattern.sentence
+    let labels = this.props.pattern.labels
+    let startPos = 0
+    let spanStarts = []
+    labels.forEach(label => {
+      if(startPos < label.startPos){
+        spanStarts.push(startPos)
+      }
+      spanStarts.push(label.startPos)
+      startPos = label.startPos + label.length
+    })
+    if (startPos < sentence.length){
+      spanStarts.push(startPos)
     }
+    return spanStarts
+  }
 
-    calcShiftPos = (spanId, offset) => {
-        let spanStartPos = this.getSpanStartPos()
-        return spanStartPos[parseInt(spanId)] + offset
+  calcShiftPos = (spanId, offset) => {
+    let spanStartPos = this.getSpanStartPos()
+    return spanStartPos[parseInt(spanId)] + offset
+  }
+
+  calcLabelPos = (startPos, endPos) => {
+    if (startPos > endPos) {
+      return { startPos: endPos, length: (startPos - endPos) }
+    } else {
+      return { startPos: startPos, length: (endPos - startPos) }
     }
+  }
 
-    calcLabelPos = (startPos, endPos) => {
-        if (startPos > endPos) {
-            return { startPos: endPos, length: (startPos - endPos) }
-        } else {
-            return { startPos: startPos, length: (endPos - startPos) }
-        }
+  selectWord = (e) => {
+    let selection = (window.getSelection) ? window.getSelection(): document.getSelection()
+
+    console.log('select word is called........')
+    if(selection.toString().length > 0){
+      let selectStartPos = this.calcShiftPos(selection.anchorNode.parentNode.id, selection.anchorOffset)
+      let selectEndPos = this.calcShiftPos(selection.focusNode.parentNode.id, selection.focusOffset)
+      this.setState({
+        showDownlist:true,
+        top: e.pageY,
+        left: e.pageX,
+        selectStartPos: selectStartPos,
+        selectEndPos:selectEndPos,
+        length: selection.toString().length,
+        sentence: selection.toString()
+      })
     }
+  }
 
-    selectWord = (e) => {
-        let selection = (window.getSelection) ? window.getSelection(): document.getSelection()
-
-        console.log('select word is called........')
-        if(selection.toString().length > 0){
-            let selectStartPos = this.calcStartPos(selection.anchorNode.parentNode.id, selection.anchorOffset)
-          let selectEndPos = this.calcShiftPos(selection.focusNode.parentNode.id, selection.focusOffset)
-          this.setState({
-            showDownlist:true,
-            top: e.pageY,
-            left: e.pageX,
-            selectStartPos: selectStartPos,
-            selectEndPos:selectEndPos,
-            length: selection.toString().length,
-            sentence: selection.toString()
-          })
-        }
-    }
-
-    removePattern = () => {
-        this.props.removePatternBy(this.props.patternId)
-    }
+  removePattern = () => {
+    this.props.removePatternBy(this.props.patternId)
+  }
 
   hideDownlist = () => {
-        this.setState({
-          showDownlist: false
-        })
+    this.setState({
+      showDownlist: false
+    })
   }
 
   entityOrPhrase = (obj) => {
-    this.props.updateSelectLabel(this.props.patternId, { ...this.calcLabelPos(selectStartPos, selectEndPos),id:obj.id,type:obj.type})
+
+    this.props.updateSelectLabel(this.props.patternId, { ...this.calcLabelPos(this.state.selectStartPos, this.state.selectEndPos),id:obj.id,type:obj.type})
   }
-    
-    render() {
-  return (<Row>
-  <Col span={20}>
-  <p onMouseUp={this.selectWord}>{this.getSpans()}</p>
-  </Col>
-  <Col span={4}>
-  <Button onClick={this.removePattern}  icon="close"></Button>
-  </Col>
-    {
-      this.state.showDownlist?<ColorDownList top={this.state.top} sentence={this.state.sentence} left={this.state.left} intent={this.props.intent} agent={this.props.agent} intentId={this.props.intentId} hideDownlist={this.hideDownlist} entityOrPhrase={this.entityOrPhrase}/>:''
-    }
-  </Row>)
-    }
+
+  render() {
+    return (<Row>
+      <Col span={20}>
+        <p onMouseUp={this.selectWord}>{this.getSpans()}</p>
+      </Col>
+      <Col span={4}>
+        <Button onClick={this.removePattern}  icon="close"></Button>
+      </Col>
+      {
+        this.state.showDownlist?<ColorDownList top={this.state.top} sentence={this.state.sentence} left={this.state.left} intent={this.props.intent} agent={this.props.agent} intentId={this.props.intentId} hideDownlist={this.hideDownlist} entityOrPhrase={this.entityOrPhrase}/>:''
+      }
+    </Row>)
+  }
 }
 
-PatternLine.propTypes = { 
-    patternId: React.PropTypes.number,
-    removePatternBy: React.PropTypes.func,
-    pattern :React.PropTypes.object,
-    updateSelectLabel: React.PropTypes.func};
+PatternLine.propTypes = {
+  patternId: React.PropTypes.number,
+  removePatternBy: React.PropTypes.func,
+  pattern :React.PropTypes.object,
+  updateSelectLabel: React.PropTypes.func};
